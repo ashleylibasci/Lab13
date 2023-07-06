7b91f18255513d452da13f7c3d45aec6cd90ef1c
Your Name <youremail@yourdomain.com>
2023-07-06 11:14:40 -0500

    update

--- src/Lab13.cpp
+++ src/Lab13.cpp
@@ -2,16 +2,66 @@
 //       THIS IS A GENERATED FILE - DO NOT EDIT       //
 /******************************************************/
 
-#include "Particle.h"
 #line 1 "/Users/ashleylibasci/IoTFolderLabs/Lab13/src/Lab13.ino"
+#include "oled-wing-adafruit.h"
+#include "Particle.h"
 
 void setup();
 void loop();
-#line 2 "/Users/ashleylibasci/IoTFolderLabs/Lab13/src/Lab13.ino"
-void setup() {
- 
+#line 4 "/Users/ashleylibasci/IoTFolderLabs/Lab13/src/Lab13.ino"
+SYSTEM_THREAD(ENABLED);
+
+OledWingAdafruit display;
+
+#include "MQTT.h"
+
+void callback(char *topic, byte *payload, unsigned int length);
+
+MQTT client("lab.thewcl.com", 1883, callback);
+
+void setup()
+{
+  Serial.begin(9600);
+
+  display.setup();
+  display.clearDisplay();
+  display.display();
 }
 
-void loop() {
- 
+void loop()
+{
+  display.loop();
+
+  if (client.isConnected())
+  {
+    client.loop();
+  }
+  else
+  {
+    client.connect(System.deviceID());
+    client.subscribe("ashleyLab13");
+    client.publish("ashleyLab13", "welcome!!");
+  }
+}
+
+void callback(char *topic, byte *payload, unsigned int length)
+{
+  char p[length + 1];
+  memcpy(p, payload, length);
+  p[length] = NULL;
+  Serial.printf("%s", p);
+  Serial.println();
+
+  if (String(p).equals("1"))
+  {
+    Serial.println("1");
+  }
+  else if (String(p).equals("0"))
+  {
+    
+  }
+  else
+  {
+    Serial.println(p);
+  }
 }
--- src/Lab13.ino
+++ src/Lab13.ino
@@ -1,8 +1,59 @@
+#include "oled-wing-adafruit.h"
+#include "Particle.h"
 
-void setup() {
- 
+SYSTEM_THREAD(ENABLED);
+
+OledWingAdafruit display;
+
+#include "MQTT.h"
+
+void callback(char *topic, byte *payload, unsigned int length);
+
+MQTT client("lab.thewcl.com", 1883, callback);
+
+void setup()
+{
+  Serial.begin(9600);
+
+  display.setup();
+  display.clearDisplay();
+  display.display();
 }
 
-void loop() {
- 
+void loop()
+{
+  display.loop();
+
+  if (client.isConnected())
+  {
+    client.loop();
+  }
+  else
+  {
+    client.connect(System.deviceID());
+    client.subscribe("ashleyLab13");
+    client.publish("ashleyLab13", "welcome!!");
+  }
+}
+
+void callback(char *topic, byte *payload, unsigned int length)
+{
+  char p[length + 1];
+  memcpy(p, payload, length);
+  p[length] = NULL;
+  Serial.printf("%s", p);
+  Serial.println();
+
+  if (String(p).equals("1"))
+  {
+    Serial.println("1");
+  }
+  else if (String(p).equals("0"))
+  {
+    
+  }
+  else
+  {
+    Serial.println(p);
+  }
 }
